de5e788b0c51d6a5d8621c92b079800a3dcd4ac6
Rashmi Sewwandi <rashmiswa09@gmail.com>
2022-08-15 20:32:22 +0530

    log in form

---
+++ client/src/controller/LoginFormController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+
+
+    public void logInOnAction(ActionEvent actionEvent) throws IOException {
+        if (txtUserName.getText().equals("client") && txtPassword.getText().equals("1234")) {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientForm.fxml"))));
+            stage.setTitle("Play Tech Chat App  - Client Chat ");
+            stage.show();
+
+        /*} else if (txtUserName.getText().equals("client1") && txtPassword.getText().equals("1234")) {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientForm1.fxml"))));
+            stage.setTitle("Play Tech Chat App  - Client Chat 01");
+            stage.show();
+
+        } else if (txtUserName.getText().equals("client2") && txtPassword.getText().equals("1234")) {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientForm2.fxml"))));
+            stage.setTitle("Play Tech Chat App  - Client Chat 02");
+            stage.show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Please Try Again").show();
+        }*/
+        }
+    }
+}
