95320fbeed1aeafbf9e2404e52a95a25e381b12d
Rashmi Sewwandi <rashmiswa09@gmail.com>
2022-08-16 11:29:32 +0530

    add client 2 and client3

---
+++ client/src/controller/ClientForm2Controller.java
@@ -0,0 +1,51 @@
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientForm2Controller {
+    static Socket socket = null;
+    static DataInputStream dataInputStream;
+    static DataOutputStream dataOutputStream;
+    public TextArea txtClientArea1;
+    public TextField txtClientOne;
+
+    String messageIn = "";
+
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost", 5000);
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (!messageIn.equals("end")) {
+                    messageIn = dataInputStream.readUTF();
+                    txtClientArea1.appendText("\nServer :" + messageIn.trim() + "\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        String reply = "";
+        reply = txtClientOne.getText();
+        txtClientArea1.appendText(("\t\t\t\t\t\t\t\t ClientTwo:" + reply.trim()));
+        dataOutputStream.writeUTF(reply);
+        txtClientOne.setText("");
+    }
+
+    public void getPhoto(MouseEvent mouseEvent) throws IOException {
+
+    }
+}
---
+++ server/src/controller/ServerSideController.java
@@ -0,0 +1,50 @@
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSideController {
+    static ServerSocket serverSocket;
+    static DataInputStream dataInputStream;
+    static DataOutputStream dataOutputStream;
+    static Socket socket;
+    public TextField txtServerOne;
+    public TextArea txtServerArea1;
+
+    String messageIn = "";
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(5000);
+                System.out.println("Server Started Waiting for client! .....");
+                socket = serverSocket.accept();
+                System.out.println("Client Accepted! .......");
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (!messageIn.equals("end")) {
+                    messageIn = dataInputStream.readUTF();
+                    txtServerArea1.appendText("\nClient:" + messageIn.trim() + "\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        String text = txtServerOne.getText();
+        txtServerArea1.appendText("\t\t\t\t\t\t\t\tServer:" +text.trim());
+        dataOutputStream.writeUTF(text);
+        txtServerOne.setText("");
+    }
+}
